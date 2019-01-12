9eb248641288d70c7445830515c51d1f26ab57e8
Ilya <ki.lel@yandex.ru>
2019-01-12 14:07:48 +0300

    UC-22: Added bootstrap styles, changed template engine to thymeleaf (#25)
    * UC-22: Added bootstrap styles, changed template engine to thymeleaf
    
    * UC-22: minor fix
    
    * UC-22: minor fix

--- src/main/java/org/github/unicon/web/MainController.java
+++ src/main/java/org/github/unicon/web/MainController.java
@@ -9,61 +9,55 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-
 @Controller
 public class MainController {
 
     @RequestMapping("/")
     public String main(Model model) {
-        model.addAttribute("types", MeasureType.values());
+        fillCommonModelFields(model);
         return "main";
     }
 
-    @RequestMapping("/convert/simple")
-    public String convertSimple(Model model,
-                                @RequestParam("type") MeasureType type) {
-        MeasureType[] allTypes = MeasureType.values();
-        ArrayList<MeasureType> types = new ArrayList<>();
-        for (MeasureType allType : allTypes) {
-            if (allType != type) {
-                types.add(allType);
-            }
-        }
-
-        model.addAttribute("types", types);
+    @RequestMapping("/convert/measure")
+    public String convertMeasure(Model model,
+                                 @RequestParam("type") MeasureType type) {
+        fillCommonModelFields(model);
         model.addAttribute("units", type.getUnits());
         model.addAttribute("currentType", type);
 
-        return "convert-simple";
+        return "convert/measure";
     }
 
     @RequestMapping("/convert/date")
     public String convertDate(Model model) {
-        model.addAttribute("types", MeasureType.values());
+        fillCommonModelFields(model);
         model.addAttribute("units", MeasureType.DURATION.getUnits());
-        return "convert-date";
+        return "convert/date";
     }
 
     @RequestMapping("/convert/escape")
     public String convertEscape(Model model) {
-        model.addAttribute("types", MeasureType.values());
+        fillCommonModelFields(model);
         model.addAttribute("esTypes", EscapeType.values());
-        return "convert-escape";
+        return "convert/escape";
     }
 
     @RequestMapping("/convert/encode")
     public String convertEncode(Model model) {
-        model.addAttribute("types", MeasureType.values());
+        fillCommonModelFields(model);
         model.addAttribute("encodeTypes", EncodingType.values());
-        return "convert-encode";
+        return "convert/encode";
     }
 
     @RequestMapping("/convert/hash")
     public String convertHash(Model model) {
-        model.addAttribute("types", MeasureType.values());
+        fillCommonModelFields(model);
         model.addAttribute("encodeTypes", EncodingType.values());
         model.addAttribute("hashTypes", HashType.values());
-        return "convert-hash";
+        return "convert/hash";
+    }
+
+    private void fillCommonModelFields(Model model) {
+        model.addAttribute("measureTypes", MeasureType.values());
     }
 }
